f2028e5465d82c1d8dc92b257ae0bfb89c887435
Trong <Trong>
2021-11-18 06:29:52 +0700

    updatefooter

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function Footer(props) {
+    return (
+        <div className="footer">
+            <div className='row'>
+                <div className="col-3">
+                    <h5 className='font-weight-bold'>GIỚI THIỆU</h5>
+                    <p>Phương thức hoạt động của AirBnB</p>
+                    <p>Trang tin tức</p>
+                    <p>Nhà đầu tư</p>
+                    <p>AirBnB Plus</p>
+                    <p>AirBnB Luxe</p>
+                    <p>Hotel Tonight</p>
+                    <p>AirBnB for Work</p>
+                    <p>Nhờ có Host,mọi điều đều có thể</p>
+                    <p>Cơ hội nghề nghiệp</p>
+                    <p>Thư của nhà sáng lập</p>
+                </div>
+                <div className="col-3">
+                    <h5 className='font-weight-bold'>CỘNG ĐỒNG</h5>
+                    <p>Sự đa dạng và cảm giác thân thuộc</p>
+                    <p>Tiện nghi phù hợp cho người khuyết tật</p>
+                    <p>Đối tác liên kết AirBnB</p>
+                    <p>Chỗ ở cho tuyến đầu</p>
+                    <p>Lượt giới thiệu của khách</p>
+                    <p>AirBnB.org</p>
+                </div>
+                <div className="col-3">
+                    <h5 className='font-weight-bold'>ĐÓN TIẾP KHÁCH</h5>
+                    <p>Cho thuê nhà</p>
+                    <p>Tổ chức trải nghiệm trực tuyến</p>
+                    <p>Tổ chức trải nghiệm</p>
+                    <p>Đón tiếp khách có trách nhiệm</p>
+                    <p>Trung tâm tài nguyên</p>
+                    <p>Trung tâm cộng đồng</p>
+                </div>
+                <div className="col-3">
+                    <h5 className='font-weight-bold'>HỖ TRỢ</h5>
+                    <p>Biện pháp ứng phó với đại dịch COVID-19 của chúng tôi</p>
+                    <p>Trung tâm trợ giúp</p>
+                    <p>Các tùy chọn hủy</p>
+                    <p>Hỗ trợ khu dân cư</p>
+                    <p>Tin cậy và an toàn</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/templates/HomeTemplate/HomeTemplate.js
+++ src/templates/HomeTemplate/HomeTemplate.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import { Route } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import CarouselHome from "../../components/CarouselHome/CarouselHome";
-
+import Footer from "../../components/Footer/Footer";
 export default function HomeTemplate(props) {//path,component,exact
     //Destructuring
     // const {Component,...restProps} = props;
@@ -14,9 +14,7 @@ export default function HomeTemplate(props) {//path,component,exact
                 <CarouselHome {...propsRoute}/>
                 {/* <props.component{...propsRoute} /> */}
 
-                <footer className='bg-dark h-10'>;
-                    Đây là footer HomePage
-                </footer>
+                <Footer/>
             </Fragment>
         }} />
     </Fragment>
